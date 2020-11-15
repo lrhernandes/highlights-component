00086bf043b4b3a1407c842c6416a1ed58e50acb
lrhernandes <larachernandes@gmail.com>
2020-11-15 02:41:16 -0300

    feat(json server): cards placed whith json server

--- highlights/src/templates/AttributesDetails/index.js
+++ highlights/src/templates/AttributesDetails/index.js
@@ -7,20 +7,37 @@ import parkingIcon from '../../img/sedan.svg';
 import Attribute from '../Attribute';
 
 export default function AttributesDetails({card}){
+
     const sizeValue = card.attributes.size;
     const suitesValue = card.attributes.suites;
-    const bedroomValue = card.attributes.bedroom;
+    const bedroomValue = card.attributes.bedrooms;
     const parkingValue = card.attributes.parking;
 
     const suitsDtails = (suitesValue>1) ? ("suites") : ("suite");
     const bedroomDtails = (bedroomValue>1) ? ("dorms") : ("dorm");
     const parkingDtails = (parkingValue>1) ? ("vagas") : ("vaga");
+
+    const iconArray = [sizeIcon, suiteIcon, bedroomIcon, parkingIcon];
+    const valuesArray = [sizeValue, suitesValue, bedroomValue, parkingValue];
+    const dtailsArray = ["M²", suitsDtails, bedroomDtails, parkingDtails];
+
     return(
         <div className="attributes-details">
+            {
+                iconArray.map((icon, index) =>{
+                    if(valuesArray[index]){
+                        return(<Attribute icon={iconArray[index]} value={valuesArray[index]} dtails={dtailsArray[index]}/>);
+                    }
+                })
+            }   
+        </div>
+    )
+}
+
+/*
+
             <Attribute icon={sizeIcon} value={sizeValue} dtails={"M²"}/>
             <Attribute icon={suiteIcon} value={suitesValue} dtails={suitsDtails}/>
             <Attribute icon={bedroomIcon} value={bedroomValue} dtails={bedroomDtails}/>
             <Attribute icon={parkingIcon} value={parkingValue} dtails={parkingDtails}/>
-        </div>
-    )
-}
+*/
