18bd0f975d3dddb6d5692aac1bdf9f1f1ec6645c
lrhernandes <larachernandes@gmail.com>
2020-11-15 17:22:48 -0300

    fix(img): src

--- highlights/src/templates/HighlightsCard/index.js
+++ highlights/src/templates/HighlightsCard/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './styles.css';
-import banner2 from '../../img/img.jpg';
 import Details from '../DetailsCard';
 
 export default function HighlightsCard({card}){
     return(
         <div className="highlights-card">
-            <img className="card-banner" src={banner2}/>
+            <img className="card-banner" src={card.img}/>
             <div className="detais-card">
                 <Details card={card}/>
             </div>
